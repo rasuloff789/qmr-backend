f2e69af575ba1736e5c1c60f8f1babde2abd7830
rasuloff789 <rasuloff789@gmail.com>
2025-10-21 02:48:24 +0500

    add login root and hashing password

--- src/modules/queries/login.root.js
+++ src/modules/queries/login.root.js
@@ -1,14 +1,35 @@
 import prisma from "../../config/db.js"; // This imports the Prisma client
+import { signToken } from "../../utils/jwt.js";//Function to sign JWT tokens
+import { verifyPassword } from "../../utils/hashpswrd.js";
 
 // loginRoot resolver is used for authenticating root users
 
 const loginRoot = async (_parent, { username, password }) => {
-    const user = await prisma.root.findUnique({ where: { username } });
-    console.log(user);
-    if (!user || user.password !== password) {
-      throw new Error("Invalid credentials");
-    }
-    return "user";  
+
+  const user = await prisma.root.findUnique({ where: { username } });
+
+  const loginResponse = {
+    "success": false,
+    "message": "Incorrect username or password in ROOT login",
+    "token": null,
   };
 
- export { loginRoot }; 
+  if (!user) {
+    return loginResponse
+  } else{
+    const isValid = await verifyPassword(password, user.password);
+    if (!isValid) {
+      return loginResponse
+    }
+  }
+
+
+  const token = signToken({ id: user.id, role: "ROOT" });
+
+  loginResponse.success = true;
+  loginResponse.message = "You have successfully logged in as ROOT";
+  loginResponse.token = token;
+  return loginResponse;  
+  };
+
+export { loginRoot }; 
---
+++ src/utils/hashpswrd.js
@@ -0,0 +1,19 @@
+// This file for hashing passwords (if needed in future)
+// And checking hashed passwords
+
+import bcrypt from "bcrypt";
+
+// password hash qilish
+const hashPassword = async (plainPassword) => {
+  const saltRounds = 10; // standart
+  const hashed = await bcrypt.hash(plainPassword, saltRounds);
+  return hashed;
+};
+
+// passwordni tekshirish
+const verifyPassword = async (plainPassword, hashedPassword) => {
+  const isValid = await bcrypt.compare(plainPassword, hashedPassword);
+  return isValid; // true / false
+};
+
+export { hashPassword, verifyPassword };
