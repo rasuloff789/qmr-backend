6cdd3c97e71e6133cda9ae115383cd2cdbb4eb7b
rasuloff789 <rasuloff789@gmail.com>
2025-10-24 02:59:25 +0500

    get admins formatter

--- src/modules/queries/getAdmins.js
+++ src/modules/queries/getAdmins.js
@@ -2,5 +2,11 @@ import prisma from "../../config/db.js"; // This imports the Prisma client
 
 export default async function (_) {
 	const admins = await prisma.admin.findMany();
-	return admins;
+
+	const formattedAdmins = admins.map((admin) => ({
+		...admin,
+		birthDate: new Date(admin.birthDate).toLocaleDateString("en-GB"), // => "09/09/2008"
+	}));
+
+	return formattedAdmins;
 }
