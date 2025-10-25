44f7207f2168bf56ff62ace66fd51de94cf47199
rasuloff789 <rasuloff789@gmail.com>
2025-10-25 20:04:35 +0500

    refactor: remove old modules directory
    - Remove deprecated src/modules/ directory
    - All resolvers have been moved to src/graphql/resolvers/
    - Clean up old file structure

--- src/modules/queries/index.js
+++
@@ -1,7 +0,0 @@
-import getAdmins from "./getAdmins.js";
-import getAdmin from "./getAdmin.js";
-import getTeachers from "./getTeachers.js";
-import getTeacher from "./getTeacher.js";
-import me from "./me.js";
-
-export { getAdmins, getAdmin, getTeachers, getTeacher, me };
